d32836d7aa90690290a5823cc32b4285d1bcce90
ArtimNayas <sayan.mitra@shopup.org>
2022-06-13 11:44:42 +0530

    Ensure output value has 4 digit precision

--- models/calculator_test.go
+++ models/calculator_test.go
@@ -23,4 +23,16 @@ func TestCalculatorFmt(t *testing.T) {
 		calc := NewCalculator()
 		assert.Implements(t, new(fmt.Stringer), calc)
 	})
+
+	t.Run("should format with 4 digit precision", func(t *testing.T) {
+		testSamples := map[float64]string{
+			2.2: "2.2000", 2.223: "2.2230", 2.22453: "2.2245", 2.262789: "2.2628",
+		}
+		for initValue, expectedFormat := range testSamples {
+			t.Run(fmt.Sprintf("given the inital value: %0.4f", initValue), func(t *testing.T) {
+				calc := calculator{value: initValue}
+				assert.Equal(t, expectedFormat, calc.String())
+			})
+		}
+	})
 }
