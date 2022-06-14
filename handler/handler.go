4de9c04f7256b4603e9d144573760c8b5feb46bb
ArtimNayas <sayan.mitra@shopup.org>
2022-06-14 11:19:22 +0530

    Ensure handler panics if operation already exists

--- handler/handler.go
+++ handler/handler.go
@@ -2,10 +2,18 @@ package handler
 
 import (
 	"calculatorcli/models"
+	"fmt"
 )
 
 type Operation string
 type handlerFunc func(c models.Calculator, v float64)
 
+var handlers = map[Operation]handlerFunc{}
+
 func RegisterHandler(op Operation, h handlerFunc) {
+	if _, found := handlers[op]; found {
+		err := fmt.Errorf("duplicate handlers registration for Operation: %s", op)
+		panic(err)
+	}
+	handlers[op] = h
 }
--- handler/handler_test.go
+++ handler/handler_test.go
@@ -7,9 +7,24 @@ import (
 )
 
 func TestRegisterHandler(t *testing.T) {
-	t.Run("should be able to register handlers with operation and handlers func", func(t *testing.T) {
+	t.Run("should be able to register handlers with operation and handler func", func(t *testing.T) {
+		defer resetHandler()()
 		assert.NotPanics(t, func() {
 			RegisterHandler("add", func(c models.Calculator, v float64) {})
 		})
 	})
+
+	t.Run("should panic if Operation already exists", func(t *testing.T) {
+		defer resetHandler()()
+		RegisterHandler("add", func(c models.Calculator, v float64) {})
+
+		assert.Panics(t, func() {
+			RegisterHandler("add", func(c models.Calculator, v float64) {})
+		})
+	})
+}
+
+func resetHandler() func() {
+	handlers = map[Operation]handlerFunc{}
+	return func() { handlers = map[Operation]handlerFunc{} }
 }
