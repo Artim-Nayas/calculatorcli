5befa97d60aa19db3a79bb3f887f954aefa38b75
ArtimNayas <sayan.mitra@shopup.org>
2022-06-15 15:15:36 +0530

    Ensure multiplication handler output is  as desired

--- handler/multiplication_handler_test.go
+++ handler/multiplication_handler_test.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"calculatorcli/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -14,4 +17,30 @@ func TestMulHandler(t *testing.T) {
 
 	MulHandler(c, 2)
 	assert.Equal(t, "24.4600", c.String())
+}
+
+func TestMulHandlerOutput(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+
+	os.Stdout = fakeStdout
+	calc := models.NewCalculator()
+	calc.Add(1)
+	GetHandler(mulOperation)(calc, 23.22245)
+
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "23.2224", string(bytes))
+
+	r, fakeStdout, err = os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+
+	GetHandler(mulOperation)(calc, 0.01)
+	fakeStdout.Close()
+	bytes, err = io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "0.2322", string(bytes))
 }
