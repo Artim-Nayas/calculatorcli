bddfb877b77d65c145ab60ee5fc2de2ff13b0348
ArtimNayas <sayan.mitra@shopup.org>
2022-06-15 15:25:45 +0530

    Ensure division handler output is in proper format

--- handler/division_handler_test.go
+++ handler/division_handler_test.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"calculatorcli/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -14,4 +17,30 @@ func TestDivHandler(t *testing.T) {
 
 	DivHandler(c, 1000)
 	assert.Equal(t, "0.0020", c.String())
+}
+
+func TestDivHandlerOutput(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+
+	os.Stdout = fakeStdout
+	calc := models.NewCalculator()
+	calc.Add(1)
+	GetHandler(divOperation)(calc, 128)
+
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "0.0078", string(bytes))
+
+	r, fakeStdout, err = os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+
+	GetHandler(divOperation)(calc, 0.01)
+	fakeStdout.Close()
+	bytes, err = io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "0.7800", string(bytes))
 }
