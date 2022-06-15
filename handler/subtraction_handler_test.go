252e1d6605850c8fadd88f07abf9539ec7b2254a
ArtimNayas <sayan.mitra@shopup.org>
2022-06-15 14:43:16 +0530

    Ensure subtraction output format is proper

--- handler/subtraction_handler_test.go
+++ handler/subtraction_handler_test.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"calculatorcli/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -15,4 +18,29 @@ func TestSubHandler(t *testing.T) {
 
 	SubHandler(c, 13.22)
 	assert.Equal(t, "-25.4500", c.String())
+}
+
+func TestSubHandlerOutput(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+
+	os.Stdout = fakeStdout
+	calc := models.NewCalculator()
+	GetHandler(subOperation)(calc, 23.22)
+
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "-23.2200", string(bytes))
+
+	r, fakeStdout, err = os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+
+	GetHandler(subOperation)(calc, 22.23)
+	fakeStdout.Close()
+	bytes, err = io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "-45.4500", string(bytes))
 }
