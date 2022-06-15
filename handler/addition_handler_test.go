0dea6cfd52d32555b18b71d43157d5fdc86ab3c4
ArtimNayas <sayan.mitra@shopup.org>
2022-06-15 10:37:33 +0530

    Ensure Add handler output is proper

--- handler/addition_handler_test.go
+++ handler/addition_handler_test.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"calculatorcli/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -14,4 +17,29 @@ func TestAddHandler(t *testing.T) {
 
 	AddHandler(c, 13.22)
 	assert.Equal(t, "25.4500", c.String())
+}
+
+func TestAddHandlerOutput(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+
+	os.Stdout = fakeStdout
+	calc := models.NewCalculator()
+	GetHandler(addOperation)(calc, 23.22)
+
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "23.2200", string(bytes))
+
+	r, fakeStdout, err = os.Pipe()
+	require.NoError(t, err)
+	os.Stdout = fakeStdout
+
+	GetHandler(addOperation)(calc, 22.23)
+	fakeStdout.Close()
+	bytes, err = io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "45.4500", string(bytes))
 }
