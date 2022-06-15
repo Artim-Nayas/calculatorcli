ef20ecd74d8f9395c41e65eaaa3466d1731e8c39
ArtimNayas <sayan.mitra@shopup.org>
2022-06-15 22:48:32 +0530

    Ensure Cancle Handler Output is in desired format

--- handler/cancel_handler_test.go
+++ handler/cancel_handler_test.go
@@ -3,6 +3,9 @@ package handler
 import (
 	"calculatorcli/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"os"
 	"testing"
 )
 
@@ -14,4 +17,19 @@ func TestCancelHandler(t *testing.T) {
 
 	GetHandler(cancelOperation)(c, 0)
 	assert.Equal(t, "0.0000", c.String())
+}
+
+func TestCancelHandlerOutput(t *testing.T) {
+	defer setupStdout()()
+	r, fakeStdout, err := os.Pipe()
+	require.NoError(t, err)
+
+	os.Stdout = fakeStdout
+	calc := models.NewCalculator()
+	GetHandler(cancelOperation)(calc, 0)
+
+	fakeStdout.Close()
+	bytes, err := io.ReadAll(r)
+	r.Close()
+	assert.Equal(t, "0.0000", string(bytes))
 }
